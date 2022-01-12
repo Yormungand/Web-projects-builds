916f95fc0afdb0500efd82d369bc3d7b77e8b84e
turbold <turbold@sodonsolution.com>
2022-01-12 22:23:23 +0800

    dentsu: гаригуудын хөдөлгөөн, болон ажилчдын мэдээллийн хэсэг

--- src/main/dev/src/static/scrollMagic-demo/js/main.js
+++ src/main/dev/src/static/scrollMagic-demo/js/main.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     // console.clear();
-    moon_init();
+    planet_1();
+    planet_2();
+    planet_3()
     shine_1();
     shine_2();
-    Preloading_icon();
-    Rocket_launch();
+    // Preloading_icon();
+    // Rocket_launch();
 
     new fullpage("#fullpage", {
         autoScrolling: true,
@@ -33,65 +35,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     })
 })
-
-
-moon_init = function () {
-
-    const randomX = random(5, 0); // x coordinates
-    const randomY = random(55, 170); // y coordinates
-    const randomDelay = random(0, 1); // delays
-    const randomTime = random(3, 5); // random times
-    const randomTime2 = random(5, 10); // random times
-    const randomAngle = random(12, 95); // random angle
-
-    const moon = document.querySelector("#moon");
-
-    TweenLite.set(moon, {
-        x: randomX(-1),
-        y: randomX(1),
-        rotation: randomAngle(-1)
-    });
-
-    moveX(moon, 1);
-    moveY(moon, -1);
-    rotate(moon, 1);
-
-    function rotate(target, direction) {
-
-        TweenLite.to(target, randomTime2(), {
-            rotation: randomAngle(direction),
-            // delay: randomDelay(),
-            ease: Sine.easeInOut,
-            onComplete: rotate,
-            onCompleteParams: [target, direction * -1]
-        });
-    }
-
-    function moveX(target, direction) {
-
-        TweenLite.to(target, randomTime(), {
-            x: randomX(direction),
-            ease: Sine.easeInOut,
-            onComplete: moveX,
-            onCompleteParams: [target, direction * -1]
-        });
-    }
-
-    function moveY(target, direction) {
-
-        TweenLite.to(target, randomTime(), {
-            y: randomY(direction),
-            ease: Sine.easeInOut,
-            onComplete: moveY,
-            onCompleteParams: [target, direction * -1]
-        });
-    }
-
-    function random(min, max) {
-        const delta = max - min;
-        return (direction = 1) => (min + delta * Math.random()) * direction;
-    }
-}
 
 shine_1 = function () {
 
@@ -244,4 +187,180 @@ Rocket_launch = function () {
     });
 
     // MotionPathHelper.create(".astronaut");
+}
+
+
+
+planet_1 = function () {
+
+    const randomX = random(5, 0); // x coordinates
+    const randomY = random(-170, 170); // y coordinates
+    const randomDelay = random(0, 1); // delays
+    const randomTime = random(3, 5); // random times
+    const randomTime2 = random(5, 10); // random times
+    const randomAngle = random(12, 95); // random angle
+
+    const planet = document.querySelector("#planet-1");
+
+    TweenLite.set(planet, {
+        x: randomX(-1),
+        y: randomX(1),
+        rotation: randomAngle(-1)
+    });
+
+    moveX(planet, 1);
+    moveY(planet, -1);
+    rotate(planet, 1);
+
+    function rotate(target, direction) {
+
+        TweenLite.to(target, randomTime2(), {
+            rotation: randomAngle(direction),
+            // delay: randomDelay(),
+            ease: Sine.easeInOut,
+            onComplete: rotate,
+            onCompleteParams: [target, direction * -1]
+        });
+
+    }
+
+    function moveX(target, direction) {
+        TweenLite.to(target, randomTime(), {
+            x: randomX(direction),
+            ease: Sine.easeInOut,
+            onComplete: moveX,
+            onCompleteParams: [target, direction * -1]
+        });
+    }
+
+    function moveY(target, direction) {
+
+        TweenLite.to(target, randomTime(), {
+            y: randomY(direction),
+            ease: Sine.easeInOut,
+            onComplete: moveY,
+            onCompleteParams: [target, direction * -1]
+        });
+    }
+
+    function random(min, max) {
+        const delta = max - min;
+        return (direction = 1) => (min + delta * Math.random()) * direction;
+    }
+}
+
+
+planet_2 = function () {
+
+    const randomX = random(5, 0); // x coordinates
+    const randomY = random(30, 75); // y coordinates
+    const randomDelay = random(0, 0); // delays
+    const randomTime = random(30, 15); // random times
+    const randomTime2 = random(15, 30); // random times
+    const randomAngle = random(-360, 360); // random angle
+
+    const planet = document.querySelector("#planet-2");
+
+    TweenLite.set(planet, {
+        x: randomX(-1),
+        y: randomX(1),
+        rotation: randomAngle(-1)
+    });
+
+    moveX(planet, 1);
+    moveY(planet, -1);
+    rotate(planet, 1);
+
+    function rotate(target, direction) {
+
+        TweenLite.to(target, randomTime2(), {
+            rotation: randomAngle(direction),
+            // delay: randomDelay(),
+            ease: Sine.easeInOut,
+            onComplete: rotate,
+            onCompleteParams: [target, direction * -1]
+        });
+    }
+
+    function moveX(target, direction) {
+        TweenLite.to(target, randomTime(), {
+            x: randomX(direction),
+            ease: Sine.easeInOut,
+            onComplete: moveX,
+            onCompleteParams: [target, direction * -1]
+        });
+    }
+
+    function moveY(target, direction) {
+
+        TweenLite.to(target, randomTime(), {
+            y: randomY(direction),
+            ease: Sine.easeInOut,
+            onComplete: moveY,
+            onCompleteParams: [target, direction * -1]
+        });
+    }
+
+    function random(min, max) {
+        const delta = max - min;
+        return (direction = 1) => (min + delta * Math.random()) * direction;
+    }
+}
+
+
+planet_3 = function () {
+
+    const randomX = random(5, 0); // x coordinates
+    const randomY = random(30, 75); // y coordinates
+    const randomDelay = random(0, 0); // delays
+    const randomTime = random(30, 15); // random times
+    const randomTime2 = random(15, 30); // random times
+    const randomAngle = random(-360, 360); // random angle
+
+    const planet = document.querySelector("#planet-3");
+
+    TweenLite.set(planet, {
+        x: randomX(-1),
+        y: randomX(1),
+        rotation: randomAngle(-1)
+    });
+
+    moveX(planet, 1);
+    moveY(planet, -1);
+    rotate(planet, 1);
+
+    function rotate(target, direction) {
+
+        TweenLite.to(target, randomTime2(), {
+            rotation: randomAngle(direction),
+            // delay: randomDelay(),
+            ease: Sine.easeInOut,
+            onComplete: rotate,
+            onCompleteParams: [target, direction * -1]
+        });
+    }
+
+    function moveX(target, direction) {
+        TweenLite.to(target, randomTime(), {
+            x: randomX(direction),
+            ease: Sine.easeInOut,
+            onComplete: moveX,
+            onCompleteParams: [target, direction * -1]
+        });
+    }
+
+    function moveY(target, direction) {
+
+        TweenLite.to(target, randomTime(), {
+            y: randomY(direction),
+            ease: Sine.easeInOut,
+            onComplete: moveY,
+            onCompleteParams: [target, direction * -1]
+        });
+    }
+
+    function random(min, max) {
+        const delta = max - min;
+        return (direction = 1) => (min + delta * Math.random()) * direction;
+    }
 }
