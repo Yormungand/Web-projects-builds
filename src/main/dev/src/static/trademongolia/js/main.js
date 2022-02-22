ea18576f9005b65beb1aacc4c3d344467a4a9649
turbold <turbold@sodonsolution.com>
2022-02-22 18:56:27 +0800

    Mongolchamber englsih

---
+++ src/main/dev/src/static/trademongolia/js/main.js
@@ -0,0 +1,33 @@
+class Tab_interaction {
+    length = 0;
+    current = 0;
+    element = null
+    constructor(element) {
+        if (element)
+            this.element = element;
+        if (this.element !== null && this.element !== undefined) {
+            if (document.querySelector(this.element)) {
+                document.querySelector(this.element).querySelectorAll('.tab-button').forEach((el, i)=>{
+                    el.setAttribute("data-index", i);
+                    this.length++
+                });
+            } else
+                console.error("Couldn't find element with matching id");
+        } else {
+            console.error('You must specify an element with id');
+        }
+    }
+    click(that, section, mentoringType){
+        if (this.element && document.querySelector(this.element)) {
+            document.querySelector(this.element).querySelectorAll('.tab-button').forEach((el, i)=>{
+                el.classList.remove("active")
+            });
+            document.querySelector(this.element).querySelectorAll('.tab-item').forEach((el, i)=>{
+                el.classList.remove("open")
+                if (that.getAttribute("data-index") === i.toString())
+                    el.classList.add("open")
+            });
+            that.classList.add("active");
+        }
+    }
+}
