13ec4821b5e2de3283a26cce695565a61debd0af
turbold <turbold@sodonsolution.com>
2022-03-23 17:39:56 +0800

    updates

--- src/main/dev/src/static/rinchin/js/main.js
+++ src/main/dev/src/static/rinchin/js/main.js
@@ -11,6 +11,9 @@ class Tab_interaction {
                     el.setAttribute("data-index", i);
                     this.length++
                 });
+                setTimeout(()=>{
+                    document.querySelector(this.element).querySelectorAll('.tab-item.open')[0].style.opacity = 1;
+                }, 300);
             } else
                 console.error("Couldn't find element with matching id");
         } else {
@@ -23,9 +26,19 @@ class Tab_interaction {
                 el.classList.remove("active")
             });
             document.querySelector(this.element).querySelectorAll('.tab-item').forEach((el, i)=>{
-                el.classList.remove("open")
-                if (that.getAttribute("data-index") === i.toString())
-                    el.classList.add("open")
+                el.style.opacity = 0;
+                setTimeout(()=>{
+                    el.classList.remove("open")
+                }, 500)
+                if (that.getAttribute("data-index") === i.toString()) {
+                    setTimeout(()=>{
+                        el.classList.add("open");
+                        for (let opacity = 0; opacity < 1.1; opacity = opacity + 0.1)
+                        {
+                            setTimeout(function(){el.style.opacity = opacity;},200)
+                        }
+                    }, 500)
+                }
             });
             that.classList.add("active");
         }
