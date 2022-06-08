44998f5157cdd1acaeb3b339e319b2dc4ea464e6
turbold <turbold@sodonsolution.com>
2022-06-08 18:37:24 +0800

    mlc in progress

--- src/main/dev/src/static/mlc/gulpfile.js
+++ src/main/dev/src/static/mlc/gulpfile.js
@@ -29,12 +29,16 @@ function css() {
         // 'C:/Users/jturb/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/swiper/swiper-bundle.css',
         // 'C:/Users/jturb/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/@fortawesome/fontawesome-free/css/all.css',
         // 'C:/Users/jturb/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/flatpickr/dist/flatpickr.css',
+
+        '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/plyr/dist/plyr.css',
+
         '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/uikit/dist/css/uikit.css',
-        '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/globals/global.css',
         '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/fullpage.js/dist/fullpage.css',
         '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/swiper/swiper-bundle.css',
         '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/@fortawesome/fontawesome-free/css/all.css',
+        '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/perfect-scrollbar/css/perfect-scrollbar.css',
         '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/flatpickr/dist/flatpickr.css',
+        '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/globals/global.css',
         'css/*.css',
     ])
         .pipe(cleancss())
@@ -63,6 +67,12 @@ function js() {
         // 'C:/Users/jturb/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/axios/dist/axios.min.js',
         // 'C:/Users/jturb/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/gsap/dist/*.js',
         // 'C:/Users/jturb/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/flatpickr/dist/flatpickr.js',
+
+
+        '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/plyr/dist/plyr.js',
+        // '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/plyr/src/js/plyr.js',
+        // '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/plyr/src/js/preview-thumbnails.js',
+
         '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/swiper/swiper-bundle.min.js',
         '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/uikit/dist/js/uikit.min.js',
         '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/fullpage.js/dist/fullpage.extensions.min.js',
@@ -72,6 +82,7 @@ function js() {
         '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/gsap/dist/*.js',
         '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/jquery/dist/jquery.js',
         '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/flatpickr/dist/flatpickr.js',
+        '/home/turbold/IdeaProjects/Web-projects-builds/src/main/dev/node_modules/perfect-scrollbar/dist/perfect-scrollbar.js',
         'js/*.js',
     ])
         .pipe(uglify())
